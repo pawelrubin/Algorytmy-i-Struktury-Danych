21ab07f2b823cd8cf9822b0be54bd11d76ab77b6
pawelrubin <pawelrubin19@gmail.com>
2019-05-12 13:16:06 +0200

    getopt template

---
+++ laboratorium/lista4/src/com/pawelrubin/Main.java
@@ -0,0 +1,43 @@
+package com.pawelrubin;
+
+import com.sun.org.apache.xalan.internal.xsltc.cmdline.getopt.GetOpt;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            GetOpt getOpt = new GetOpt(args, "t:");
+            int c;
+            while ((c = getOpt.getNextOption()) != -1) {
+                if (c == 't') {
+                    String arg = getOpt.getOptionArg();
+                    switch (arg) {
+                        case "rbt": {
+                            System.out.println("Simulating rbt");
+                            break;
+                        }
+                        case "bst": {
+                            System.out.println("Simulating bst");
+                            break;
+                        }
+                        case "splay": {
+                            System.out.println("Simulating splay");
+                            break;
+                        }
+                        default:
+                            throw new IllegalArgumentException();
+                    }
+                } else {
+                    printUsage();
+                }
+            }
+        } catch (Exception e) {
+            printUsage();
+            System.exit(1);
+        }
+    }
+
+    private static void printUsage() {
+        System.out.println("usage: ./main [-t <rbt|bst|splay>]");
+    }
+}
