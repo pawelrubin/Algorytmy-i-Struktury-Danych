4429a217665bc85094071dee3c334c187ab94989
pawelrubin <pawelrubin19@gmail.com>
2019-05-18 00:00:17 +0200

    SplayTree cmp_count

--- laboratorium/lista4/src/com/pawelrubin/structures/SplayTree.java
+++ laboratorium/lista4/src/com/pawelrubin/structures/SplayTree.java
@@ -5,8 +5,11 @@ public class SplayTree<KeyType extends Comparable<KeyType>> extends BST<KeyType>
     @Override
     protected Node<KeyType> searchNode(Node<KeyType> x, KeyType k) {
         Node<KeyType> node = super.searchNode(x, k);
+        cmp_count++;
         if (node != null) {
+            cmp_count++;
             while (node.getParent() != null) {
+                cmp_count++;
                 this.splay(node);
             }
         }
@@ -16,8 +19,11 @@ public class SplayTree<KeyType extends Comparable<KeyType>> extends BST<KeyType>
     @Override
     protected Node<KeyType> insert(Node<KeyType> z) {
         Node<KeyType> noteToReturn = super.insert(z);
+        cmp_count++;
         if (noteToReturn != null) {
+            cmp_count++;
             while (noteToReturn.getParent() != null) {
+                cmp_count++;
                 this.splay(noteToReturn);
             }
         }
@@ -27,10 +33,16 @@ public class SplayTree<KeyType extends Comparable<KeyType>> extends BST<KeyType>
     @Override
     protected Node<KeyType> delete(Node<KeyType> z) {
         Node<KeyType> removed = super.delete(z);
-        if (removed != null && removed.getParent() != null) {
-            Node<KeyType> parent = removed.getParent();
-            while (parent.getParent() != null) {
-                this.splay(parent);
+        cmp_count++;
+        if (removed != null) {
+            cmp_count++;
+            if (removed.getParent() != null) {
+                Node<KeyType> parent = removed.getParent();
+                cmp_count++;
+                while (parent.getParent() != null) {
+                    cmp_count++;
+                    this.splay(parent);
+                }
             }
         }
         return removed;
@@ -38,115 +50,141 @@ public class SplayTree<KeyType extends Comparable<KeyType>> extends BST<KeyType>
 
     private void splay(Node<KeyType> node) {
         Node<KeyType> parent = node.getParent();
+        cmp_count++;
         Node<KeyType> grandParent = (parent != null) ? parent.getParent() : null;
-        if (parent != null && parent == root) {
-            // Zig step
-            root = node;
-            node.setParent(null);
-
-            if (node == parent.getLeft()) {
-                parent.setLeft(node.getRight());
-                if (node.getRight() != null) node.getRight().setParent(parent);
-                node.setRight(parent);
-                parent.setParent(node);
-            } else {
-                parent.setRight(node.getLeft());
-                if (node.getLeft() != null) node.getLeft().setParent(parent);
-                node.setLeft(parent);
-                parent.setParent(node);
-            }
-            return;
-        }
-        if (parent != null && grandParent != null) {
-            Node<KeyType> ggp = grandParent.getParent();
-            if (ggp != null && ggp.getLeft() == grandParent) {
-                ggp.setLeft(node);
-                node.setParent(ggp);
-            } else if (ggp != null && ggp.getRight() == grandParent) {
-                ggp.setRight(node);
-                node.setParent(ggp);
-            } else {
+        cmp_count++;
+        if (parent != null) {
+            cmp_count++;
+            if (parent == root) {
+                // Zig step
                 root = node;
                 node.setParent(null);
-            }
 
-            if ((node == parent.getLeft() && parent == grandParent.getLeft())
-                    || (node == parent.getRight() && parent == grandParent.getRight())) {
-                // Zig-zig step
+                cmp_count++;
                 if (node == parent.getLeft()) {
-                    Node<KeyType> nr = node.getRight();
+                    parent.setLeft(node.getRight());
+                    cmp_count++;
+                    if (node.getRight() != null) node.getRight().setParent(parent);
                     node.setRight(parent);
                     parent.setParent(node);
-
-                    parent.setLeft(nr);
-                    if (nr != null)
-                        nr.setParent(parent);
-
-                    Node<KeyType> pr = parent.getRight();
-                    parent.setRight(grandParent);
-                    grandParent.setParent(parent);
-
-                    grandParent.setLeft(pr);
-                    if (pr != null)
-                        pr.setParent(grandParent);
                 } else {
-                    Node<KeyType> nl = node.getLeft();
+                    parent.setRight(node.getLeft());
+                    cmp_count++;
+                    if (node.getLeft() != null) node.getLeft().setParent(parent);
                     node.setLeft(parent);
                     parent.setParent(node);
-
-                    parent.setRight(nl);
-                    if (nl != null)
-                        nl.setParent(parent);
-
-                    Node<KeyType> pl = parent.getLeft();
-                    parent.setLeft(grandParent);
-                    grandParent.setParent(parent);
-
-                    grandParent.setRight(pl);
-                    if (pl != null)
-                        pl.setParent(grandParent);
                 }
                 return;
             }
+        }
 
-            // Zig-zag step
-            if (node == parent.getLeft()) {
-                Node<KeyType> nl = node.getRight();
-                Node<KeyType> nr = node.getLeft();
+        cmp_count++;
+        if (parent != null) {
+            cmp_count++;
+            if (grandParent != null) {
+                Node<KeyType> ggp = grandParent.getParent();
+                cmp_count += 2;
+                if (ggp != null && ggp.getLeft() == grandParent) {
+                    ggp.setLeft(node);
+                    node.setParent(ggp);
+                } else if (ggp != null && ggp.getRight() == grandParent) {
+                    cmp_count += 2;
+                    ggp.setRight(node);
+                    node.setParent(ggp);
+                } else {
+                    cmp_count += 2;
+                    root = node;
+                    node.setParent(null);
+                }
 
-                node.setRight(parent);
+                if ((node == parent.getLeft() && parent == grandParent.getLeft())
+                        || (node == parent.getRight() && parent == grandParent.getRight())) {
+                    cmp_count += 4; //big brain
+                    // Zig-zig step
+                    cmp_count++;
+                    if (node == parent.getLeft()) {
+                        Node<KeyType> nr = node.getRight();
+                        node.setRight(parent);
+                        parent.setParent(node);
+
+                        parent.setLeft(nr);
+                        cmp_count++;
+                        if (nr != null)
+                            nr.setParent(parent);
+
+                        Node<KeyType> pr = parent.getRight();
+                        parent.setRight(grandParent);
+                        grandParent.setParent(parent);
+
+                        grandParent.setLeft(pr);
+                        cmp_count++;
+                        if (pr != null)
+                            pr.setParent(grandParent);
+                    } else {
+                        Node<KeyType> nl = node.getLeft();
+                        node.setLeft(parent);
+                        parent.setParent(node);
+
+                        parent.setRight(nl);
+                        cmp_count++;
+                        if (nl != null)
+                            nl.setParent(parent);
+
+                        Node<KeyType> pl = parent.getLeft();
+                        parent.setLeft(grandParent);
+                        grandParent.setParent(parent);
+
+                        grandParent.setRight(pl);
+                        cmp_count++;
+                        if (pl != null)
+                            pl.setParent(grandParent);
+                    }
+                    return;
+                }
+
+                // Zig-zag step
+                cmp_count++;
+                if (node == parent.getLeft()) {
+                    Node<KeyType> nl = node.getRight();
+                    Node<KeyType> nr = node.getLeft();
+
+                    node.setRight(parent);
+                    parent.setParent(node);
+
+                    node.setLeft(grandParent);
+                    grandParent.setParent(node);
+
+                    parent.setLeft(nl);
+                    cmp_count++;
+                    if (nl != null)
+                        nl.setParent(parent);
+
+                    grandParent.setRight(nr);
+                    cmp_count++;
+                    if (nr != null)
+                        nr.setParent(grandParent);
+                    return;
+                }
+
+                Node<KeyType> nl = node.getLeft();
+                Node<KeyType> nr = node.getRight();
+
+                node.setLeft(parent);
                 parent.setParent(node);
 
-                node.setLeft(grandParent);
+                node.setRight(grandParent);
                 grandParent.setParent(node);
 
-                parent.setLeft(nl);
+                parent.setRight(nl);
+                cmp_count++;
                 if (nl != null)
                     nl.setParent(parent);
 
-                grandParent.setRight(nr);
+                grandParent.setLeft(nr);
+                cmp_count++;
                 if (nr != null)
                     nr.setParent(grandParent);
-                return;
             }
-
-            Node<KeyType> nl = node.getLeft();
-            Node<KeyType> nr = node.getRight();
-
-            node.setLeft(parent);
-            parent.setParent(node);
-
-            node.setRight(grandParent);
-            grandParent.setParent(node);
-
-            parent.setRight(nl);
-            if (nl != null)
-                nl.setParent(parent);
-
-            grandParent.setLeft(nr);
-            if (nr != null)
-                nr.setParent(grandParent);
         }
     }
-
 }
